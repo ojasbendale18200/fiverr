552dcda8b4e93cbe7b5b5297f12e7ac3fcdaeee8
Ojas <ojasbendale18200@gmail.com>
2023-06-15 17:56:33 +0530

    redirected to orders section

--- frontend/src/components/checkoutForm/CheckoutForm.jsx
+++ frontend/src/components/checkoutForm/CheckoutForm.jsx
@@ -59,7 +59,7 @@ const CheckoutForm = () => {
       elements,
       confirmParams: {
         // Make sure to change this to your payment completion page
-        return_url: "https://fiverr-1cfm.vercel.app/success",
+        return_url: "https://fiverr-1cfm.vercel.app/orders",
       },
     });
 
