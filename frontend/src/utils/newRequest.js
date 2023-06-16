b1327fc8f75debd8b1fbed7ab1446f406fbca2ae
Ojas <ojasbendale18200@gmail.com>
2023-06-16 21:38:21 +0530

    deleting frontend

--- frontend/src/utils/newRequest.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const newRequest = axios.create({
-  baseURL: "https://fair-blue-cod-cape.cyclic.app/api/",
-  withCredentials: true,
-});
-
-export default newRequest;
